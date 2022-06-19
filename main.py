b077f208516c6bf5b25864acd4921dd54f740cd1
MrDusza <jozeksuperchlop@gmail.com>
2022-06-19 21:19:30 +0200

    early mppt ready

--- main.py
+++ main.py
@@ -19,14 +19,12 @@ def change_of_fill_factor():
     if decrement_button.value() == 0:
         if fill_value > 1:
             fill_value = fill_value - 1
-            
-    pwm.duty(fill_value * 10)            
+                        
     print('Współczynnik wypełnienia:', fill_value, '%')
     
-def power_value():
-    resistance = 6.8
+def current_value():
     mA_per_bit = 31.03
-    reference_adc_value = 809
+    reference_adc_value = 811
     reference_adc_diff = reference_adc_value - kalman_filter(adc.read())
     
     n_factor = fill_value / 100
@@ -35,20 +33,64 @@ def power_value():
     max_current_value = average_current_value / n_factor
     effective_current_value = sqrt(n_factor) * max_current_value
     
-    average_voltage_value =  (average_current_value * resistance) / 1000
-    max_voltage_value = (max_current_value * resistance) / 1000
-    effective_voltage_value = (effective_current_value * resistance) / 1000
+    #average_voltage_value =  (average_current_value * resistance) / 1000
+    #max_voltage_value = (max_current_value * resistance) / 1000
+    #effective_voltage_value = (effective_current_value * resistance) / 1000
     
-    power_value = ((effective_current_value / 1000) ** 2) * resistance
+    return effective_current_value
     
-    print('Wartość średnia prądu:', average_current_value, 'mA')
-    print('Wartość maksymalna prądu', max_current_value, 'mA')
-    print('Wartość skuteczna prądu', effective_current_value, 'mA')
-    print('Wartość średnia napięcia', average_voltage_value, 'V')
-    print('Wartość masymalna napięcia', max_voltage_value, 'V')
-    print('Wartość skuteczna napięcia', effective_voltage_value, 'V')
+    #print('Wartość średnia prądu:', average_current_value, 'mA')
+    #print('Wartość maksymalna prądu', max_current_value, 'mA')
+    #print('Wartość skuteczna prądu', effective_current_value, 'mA')
+    #print('Wartość średnia napięcia', average_voltage_value, 'V')
+    #print('Wartość masymalna napięcia', max_voltage_value, 'V')
+    #print('Wartość skuteczna napięcia', effective_voltage_value, 'V')
+
+    
+def power_value():
+    resistance = 6.8
+    power_value = ((current_value() / 1000) ** 2) * resistance
     print('Moc', power_value, 'W')
-     
+    
+    return power_value
+
+def MPPT_algorithm():
+    global fill_value
+    
+    fill_value_array = []
+    power_value_array = []
+    old_power_value = 0
+    cell_number = 0
+    
+    for fill_value_counter in range(0, 10):
+        fill_value_array.append(fill_value - 5 + fill_value_counter)
+        
+        if fill_value_array[fill_value_counter] < 2:
+            fill_value_array[fill_value_counter] = 1
+        elif fill_value_array[fill_value_counter] > 99:
+            fill_value_array[fill_value_counter] = 100
+        
+    for power_value_counter in range(0, 10):
+        fill_value = fill_value_array[power_value_counter]
+        pwm.duty(fill_value * 10)
+        for kalman_wait in range(0, 10000):
+            if kalman_wait == 9999:
+                power_value_array.append(power_value())
+                
+    for highest_value_counter in range(0, 10):
+        current_power_value = power_value_array[highest_value_counter]
+        
+        if current_power_value > old_power_value:
+            cell_number = highest_value_counter
+            old_power_value = current_power_value
+        else:
+            old_power_value = current_power_value
+            
+    fill_value = fill_value_array[cell_number]
+    pwm.duty(fill_value * 10)
+    fill_value_array.clear()
+    power_value_array.clear()
+           
 def kalman_filter(adc_value):
     global kalman_adc_old
     global P1
@@ -69,16 +111,19 @@ def kalman_filter(adc_value):
     return kalman_adc
      
 fill_value = 50
+#-------------------Kalman's settings
 kalman_adc_old = 0
 P1 = 0
-Q = 0.01        #0.0003
+Q = 1     #0.0003
 R = 5
 Kg = 0
 P = 1
-    
+#-------------------
+
 while True:
+    MPPT_algorithm()
     print('adc read', adc.read())
     change_of_fill_factor()
     power_value()
     print(' ')
-    sleep(0.1)
+    #sleep(0.1)
